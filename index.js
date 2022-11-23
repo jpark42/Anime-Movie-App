3ef396bf853d1932bec3b760eb592e8399582d1c
Johnny Park <110794898+jpark42@users.noreply.github.com>
2022-11-23 01:02:47 -0500

    Create Express code to route URL endpoints
    - updated Documentation page to list URL endpoints and the request methods that correlate with each one
    - Create Express code to route URL endpoints

--- index.js
+++ index.js
@@ -1,68 +1,195 @@
 const express = require('express'),
+  app = express();
+  bodyParser = require('body-parser');
+  uuid = require('uuid');
   morgan = require('morgan');
 
-const app = express();
+app.use(bodyParser.json()); 
 
-let topMovies = [
+
+let users = [
   {
-    Title: "Your Name",
-    Director: "Makoto Shinkai",
-    Date: 2016,
-    IMDb_Rating: "8.4/10",
+    id: 1,
+    name: "Kim",
+    favoriteMovies: ["Perfect Blue", "Your Name"],
   },
+
   {
-    Title: "Spirited Away",
-    Director: "Hayao Miyazaki",
-    Date: 2001,
-    IMDb_Rating: "8.6/10",
+    id: 2,
+    name: "Zeke",
+    favoriteMovies: ["Your Name"],
   },
+]
+
+let movies = [
   {
-    Title: "A Silent Voice",
-    Director: "Naoko Yamada",
-    Date: 2017,
-    IMDb_Rating: "8.1/10",
+    "Title": "Your Name",
+    "Description": "",
+    "Date": 2016,
+    "Genre": {
+      "Name": "Romance",
+      "Description": ""
+    },
+    "Director": {
+      "Name": "Makoto Shinkai",
+      "Bio": "",
+      "Birth": 1111,
+    },
+    "ImageURL": "",
+    "Featured": false,
   },
+
   {
-    Title: "Howl's Moving Castle",
-    Director: "Hayao Miyazaki",
-    Date: 2005,
-    IMDb_Rating: "8.2/10",
+    "Title": "Spirited Away",
+    "Description": "",
+    "Date": 2001,
+    "Genre": {
+      "Name": "Adventure",
+      "Description": ""
+    },
+    "Director": {
+      "Name": "Hayao Miyazaki",
+      "Bio": "",
+      "Birth": 1111,
+    },
+    "ImageURL": "",
+    "Featured": false,
   },
+
   {
-    Title: "Flavors of Youth",
-    Director: "Yoshitaka Takeuchi, Haoling Li, Jiaoshou Yi Xiaoxing",
-    Date: 2018,
-    IMDb_Rating: "6.6/10", 
+    "Title": "A Silent Voice",
+    "Description": "",
+    "Date": 2017,
+    "Genre": {
+      "Name": "Romance",
+      "Description": ""
+    },
+    "Director": {
+      "Name": "Naoko Yamada",
+      "Bio": "",
+      "Birth": 1111,
+    },
+    "ImageURL": "",
+    "Featured": false,
   },
+
   {
-    Title: "Weathering With You",
-    Director: "Makoto Shinkai",
-    Date: 2019,
-    IMDb_Rating: "7.5/10",
+    "Title": "Howl's Moving Castle",
+    "Description": "",
+    "Date": 2005,
+    "Genre": {
+      "Name": "Adventure",
+      "Description": ""
+    },
+    "Director": {
+      "Name": "Hayao Miyazaki",
+      "Bio": "",
+      "Birth": 1111,
+    },
+    "ImageURL": "",
+    "Featured": false,
   },
+
   {
-    Title: "Ponyo",
-    Director: "Hayao Miyazaki",
-    Date: 2008,
-    IMDb_Rating: "7.6/10",
+    "Title": "Flavors of Youth",
+    "Description": "",
+    "Date": 2018,
+    "Genre": {
+      "Name": "Romance",
+      "Description": ""
+    },
+    "Director": {
+      "Name": "Yoshitaka Takeuchi, Haoling Li, Jiaoshou Yi Xiaoxing",
+      "Bio": "",
+      "Birth": 1111,
+    },
+    "ImageURL": "",
+    "Featured": false,
   },
+
   {
-    Title: "Kiki's Delivery Service",
-    Director: "Hayao Miyazaki",
-    Date: 1990,
-    IMDb_Rating: "7.8/10",
+    "Title": "Weathering With You",
+    "Description": "",
+    "Date": 2019,
+    "Genre": {
+      "Name": "Romance",
+      "Description": ""
+    },
+    "Director": {
+      "Name": "Makoto Shinkai",
+      "Bio": "",
+      "Birth": 1111,
+    },
+    "ImageURL": "",
+    "Featured": false,
   },
+
   {
-    Title: "Perfect Blue",
-    Director: "Satoshi Kon",
-    Date: 1997,
-    IMDb_Rating: "8/10",
+    "Title": "Ponyo",
+    "Description": "",
+    "Date": 2008,
+    "Genre": {
+      "Name": "Adventure",
+      "Description": ""
+    },
+    "Director": {
+      "Name": "Hayao Miyazaki",
+      "Bio": "",
+      "Birth": 1111,
+    },
+    "ImageURL": "",
+    "Featured": false,
   },
+
   {
-    Title: "Akira",
-    Director: "Katsuhiro Otomo",
-    Date: 1988,
-    IMDb_Rating: "8/10",
+    "Title": "Kiki's Delivery Service",
+    "Description": "",
+    "Date": 1990,
+    "Genre": {
+      "Name": "Adventure",
+      "Description": "",
+    },
+    "Director": {
+      "Name": "Hayao Miyazaki",
+      "Bio": "",
+      "Birth": 1111,
+    },
+    "ImageURL": "",
+    "Featured": false,
+  },
+
+  {
+    "Title": "Perfect Blue",
+    "Description": "",
+    "Date": 1997,
+    "Genre": {
+      "Name": "Thriller",
+      "Description": ""
+    },
+    "Director": {
+      "Name": "Satoshi Kon",
+      "Bio": "",
+      "Birth": 1111,
+    },
+    "ImageURL": "",
+    "Featured": false,
+  },
+
+  {
+    "Title": "Akira",
+    "Description": "",
+    "Date": 1988,
+    "Genre": {
+      "Name": "Sci-fi",
+      "Description": ""
+    },
+    "Director": {
+      "Name": "Katsuhiro Otomo",
+      "Bio": "",
+      "Birth": 1111,
+    },
+    "ImageURL": "",
+    "Featured": false,
   },
 ]
 
@@ -76,8 +203,122 @@ app.get('/', (req, res) => {
   res.send('Welcome to AnimeFlix!');
 });
 
+//CREATE
+app.put('/users/:id', (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser)
+  } else {
+    res.status(400).send('users need names')
+  }
+});
+
+//UPDATE
+app.post('/users', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  //2 equal signs because user.id refers to a number and id refers to a string. This makes it truthy and equal to satisfy the condition
+  let user = users.find( user => user.id == id );  
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send('no such user');
+  }
+});
+
+//CREATE
+app.post('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find( user => user.id == id );  
+
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+  } else {
+    res.status(400).send('no such user');
+  }
+});
+
+//DELETE
+app.delete('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find( user => user.id == id );  
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
+    res.status(200).send(`${movieTitle} has been removoed from user ${id}'s array`);
+  } else {
+    res.status(400).send('no such user');
+  }
+});
+
+//DELETE
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find( user => user.id == id );  
+
+  if (user) {
+    users = users.filter( user => user.id != id);
+    res.status(200).send(`User ${id} has been deleted`);
+  } else {
+    res.status(400).send('no such user');
+  }
+});
+
+//READ
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.status(200).json(movies);
+});
+
+
+//READ
+app.get('/movies/:title', (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find(movie => movie.Title === title);
+
+  if (movie) {
+    return res.status(200).json(movie);
+  }
+  else {
+    res.status(400).send('no such movie');
+  }
+});
+
+
+//READ
+app.get('/movies/genre/:genreName', (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
+
+  if (genre) {
+    return res.status(200).json(genre);
+  }
+  else {
+    res.status(400).send('no such genre');
+  }
+});
+
+
+//READ
+app.get('/movies/director/:directorName', (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find(movie => movie.Director.Name === directorName).Director;
+
+  if (director) {
+    return res.status(200).json(director);
+  }
+  else {
+    res.status(400).send('no such director');
+  }
 });
 
 // Error
