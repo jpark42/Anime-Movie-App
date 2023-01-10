8f552aa076b48517364d15330ca53b42dae9e0ec
Johnny Park <110794898+jpark42@users.noreply.github.com>
2023-01-09 22:25:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -209,7 +209,7 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
 
 
 // Get list of all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', /*passport.authenticate('jwt', { session: false }),*/ (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(200).json(movies);
