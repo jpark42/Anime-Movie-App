591da39d70fa3df07c2c35e9d59306978f8ae912
Johnny Park <110794898+jpark42@users.noreply.github.com>
2023-01-10 19:00:31 -0500

    Allow Access to Origin
    - Allowed access to another localhost origin

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234/'];
 
 app.use(cors({
   origin: (origin, callback) => {
